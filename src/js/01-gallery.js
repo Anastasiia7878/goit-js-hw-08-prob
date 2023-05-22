cfd21c37296e190a49672df04c5056d7bb52ccb6
Anastasiia7878 <dr.saymon78@gmail.com>
2023-05-22 22:14:20 +0300

    Update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryList = document.querySelector(".gallery");
+
+
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
+
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
+
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  lightbox.open();
+});
